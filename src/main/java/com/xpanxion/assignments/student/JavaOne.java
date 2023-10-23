e08359665ae481bb33c209d4bb82a5838cd6eed2
Herman <julian.herman@fiserv.com>
2023-10-23 10:06:15 -0400

    implemented ex5

--- src/main/java/com/xpanxion/assignments/student/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student/JavaOne.java
@@ -15,16 +15,15 @@ public class JavaOne {
     // Public methods
     //
 
+    public static final Scanner console = new Scanner(System.in);
+
     public void ex1() {
-        Scanner console = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = console.nextLine();
         System.out.println("Your name in upper case is: " + name.toUpperCase());
-        console.close();
     }
 
     public void ex2() {
-        Scanner console = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String someString = console.nextLine();
         int countUpperCase = 0;
@@ -34,11 +33,9 @@ public class JavaOne {
         }
 
         System.out.println("Number of uppercase letters: " + countUpperCase);
-        console.close();
     }
 
     public void ex3() {
-        Scanner console = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String someString = console.nextLine();
         StringTokenizer st = new StringTokenizer(someString);
@@ -57,11 +54,9 @@ public class JavaOne {
         }
 
         System.out.println(result);
-        console.close();
     }
 
     public void ex4() {
-        Scanner console = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String someString = console.nextLine();
         char[] someStringCharArr = someString.toCharArray();
@@ -76,11 +71,32 @@ public class JavaOne {
         }
 
         System.out.println("IS A PALINDROME");
-        console.close();
     }
 
     public void ex5() {
-        System.out.println("Student 1: ex5.");
+        String someString;
+        char[] charsOfSomeString;
+        int numVowels, numConsonants;
+        while ( true ) {
+            numVowels=0;
+            numConsonants=0;
+            System.out.print("Please enter a string: ");
+            someString = console.nextLine();
+            if ( someString.compareToIgnoreCase("quit") == 0 ) break;
+            charsOfSomeString = someString.toCharArray();
+            for ( char c : charsOfSomeString ) {
+                if ( Character.isLetter(c) ) {
+                    c = Character.toLowerCase(c);
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                        numVowels++;
+                    } else {
+                        numConsonants++;
+                    }
+                }
+            }
+            System.out.println("Number of vowels: " + numVowels);
+            System.out.println("Number of consonants: " + numConsonants);
+        }
     }
 
     public void ex6() {
